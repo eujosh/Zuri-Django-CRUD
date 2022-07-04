984326c4fba1f34eaea4b105f33d4da0fe9cce4a
“Joshua <umukorojosh@gmail.com>
2022-07-04 17:03:58 +0100

    View and templates created

--- I4G006878PQF/blog/views.py
+++ I4G006878PQF/blog/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from . models import Post
 from django.views import generic
+from django.urls import reverse_lazy
 
-from I4G006878PQF.blog.models import Post
-
-# Create your views here.
 class PostListView(generic.ListView):
     model = Post
 
 class PostCreateView(generic.CreateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
-    
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
 class PostDetailView(generic.DetailView):
     model = Post
 
 class PostUpdateView(generic.UpdateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
 
 class PostDeleteView(generic.UpdateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
